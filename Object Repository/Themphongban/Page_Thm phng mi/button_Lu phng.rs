57b503fb4e43c150220f5919f625b2a5f878d867
vananh1551998 <vananh1551998@gmail.com>
2020-12-25 23:40:44 +0700

    change for test on jenkins

---
+++ Object Repository/Themphongban/Page_Thm phng mi/button_Lu phng.rs
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Lu phng</name>
+   <tag></tag>
+   <elementGuidId>913617e0-c1e6-4cb9-bba8-cdad38acdf98</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn.btn-primary.btn-large</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary btn-large</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> Lưu phòng</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;formIdPr&quot;)/div[@class=&quot;panel-body Tlabel-Edit&quot;]/div[@class=&quot;form-group row form-body&quot;]/div[@class=&quot;TcssButton&quot;]/button[@class=&quot;btn btn-primary btn-large&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@type='submit']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='formIdPr']/div/div[2]/div/button</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Địa chỉ'])[1]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Trạng thái'])[1]/following::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hủy'])[1]/preceding::button[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Lưu phòng']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Themphongban.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Themphongban</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>44c8b58b-2501-4a54-adeb-84254cedd20c</testSuiteGuid>
+   <testCaseLink>
+      <guid>cd266d21-ced1-429b-ac36-dc25a666b657</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test phongban</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e27834c3-4934-4f0a-aa96-20f5eb8f6614</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Themphongban</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>e27834c3-4934-4f0a-aa96-20f5eb8f6614</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Mã Phòng </value>
+         <variableId>feafc4d6-b592-416f-93f4-a8419dc7ef2c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>e27834c3-4934-4f0a-aa96-20f5eb8f6614</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Tên Phòng </value>
+         <variableId>a7376b9d-60dd-40b7-8a8d-f6db1eac2f20</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>e27834c3-4934-4f0a-aa96-20f5eb8f6614</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Địa Chỉ </value>
+         <variableId>92702633-f285-44fc-9727-12541b40bfd8</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
